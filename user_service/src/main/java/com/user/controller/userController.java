d697a40aa5916394ebee4058e5ba77330fed2613
santhoshn-vrize <Santhosh@vrize.com>
2024-03-26 12:21:33 +0530

    integrating user_Service with contact

--- user_service/src/main/java/com/user/controller/userController.java
+++ user_service/src/main/java/com/user/controller/userController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -14,8 +17,16 @@ public class userController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RestTemplate restTemplate;
     @GetMapping("/{userId}")
     public User getUser(@PathVariable("userId") Long userId){
-        return this.userService.getUser(userId);
+        User user = this.userService.getUser(userId);
+
+       List contacts = this.restTemplate.getForObject("http://localhost:9002/contact/user/" + user.getUserId(), List.class);
+
+       user.setContacts(contacts);
+        return user;
     }
 }
